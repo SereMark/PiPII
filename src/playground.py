f9971e881a76d973b2aa4128cbfbe0d6690665fe
SereMark <seremark056@gmail.com>
2025-03-05 14:11:33 +0100

    removed unallowed libraries

--- src/playground.py
+++
@@ -1,7 +0,0 @@
-class PlaygroundWorker:
-    def __init__(self, progress_callback, status_callback):
-        self.progress_callback = progress_callback
-        self.status_callback = status_callback
-
-    def run(self):
-        raise NotImplementedError("PlaygroundWorker.run() is not implemented yet.")
